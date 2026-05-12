3771badf9b43938c60b7d57ee13afaca9db1d4f2
김용진 <kimjin8748@gmail.com>
2026-05-12 17:51:05 +0900

    05.12_WildCardTest.java

---
+++ Hello_Java/src/WildCardTest/WildCardTest.java
@@ -0,0 +1,36 @@
+package WildCardTest;
+
+import java.util.ArrayList;
+
+public class WildCardTest {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		for(int i = 0; i < 5; i++) {
+			intList.add(i);
+		}
+		ArrayList<Double> doubleList = new ArrayList<Double>();
+		for(int i = 0; i < 5; i++) {
+			doubleList.add((double)i);
+		}
+		ArrayList<Number> numList = new ArrayList<Number>();
+		for(int i = 0; i < 5; i++) {
+			numList.add((double)i);
+			numList.add(i);
+		}
+		System.out.println(sum(intList));
+		System.out.println(sum(doubleList));
+		System.out.println(sum(numList));
+	}
+	
+	public static double sum(ArrayList<? extends Number> list) {
+		double total = 0;
+		
+		for (Number num : list) {
+            total += num.doubleValue();
+        }
+		
+		return total;
+	}
+
+}
